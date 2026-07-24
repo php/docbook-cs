NickSdot/php__docbook-cs

feat: add automatic fixers

tests/Integration/Fix/ExceptionNameFixerTest.php
@@ -1,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Integration\Fix;
+
+use DocbookCS\Fix\Fix;
+use DocbookCS\Fix\FixApplier;
+use DocbookCS\Fix\FixPlan;
+use DocbookCS\Fix\Fixer\ExceptionNameFixer;
+use DocbookCS\Fix\FixResult;
+use DocbookCS\Runner\EntityExpansionMarker;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Sniff\ExceptionNameSniff;
+use DocbookCS\Source\File;
+use DocbookCS\Source\Line;
+use DocbookCS\Violation\SourceRange;
+use DocbookCS\Violation\Violation;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[
+    CoversClass(ExceptionNameFixer::class),
+    CoversClass(ExceptionNameSniff::class),
+    CoversClass(Fix::class),
+    CoversClass(FixApplier::class),
+    CoversClass(FixResult::class),
+    CoversClass(RunMode::class),
+    CoversClass(Violation::class),
+    //
+    UsesClass(EntityExpansionMarker::class),
+    UsesClass(File::class),
+    UsesClass(FixPlan::class),
+    UsesClass(Line::class),
+    UsesClass(SourceRange::class),
+]
+final class ExceptionNameFixerTest extends TestCase
+{
+    #[Test]
+    public function itReplacesSimpleClassnameTags(): void
+    {
+        $content = '<root><classname>RuntimeException</classname></root>';
+        $document = $this->createDocument($content);
+        $source = new File('file.xml', $content);
+
+        $violations = new ExceptionNameSniff(RunMode::Fix)->process($document, $source);
+
+        $beginOffset = (int) strpos($content, '<classname>');
+        $sourceContent = '<classname>RuntimeException</classname>';
+
+        self::assertCount(1, $violations);
+        self::assertSame($sourceContent, $violations[0]->content);
+        self::assertSame($beginOffset, $violations[0]->beginOffset);
+        self::assertSame($beginOffset + strlen($sourceContent), $violations[0]->untilOffset);
+        self::assertSame(1, $violations[0]->line);
+
+        $fix = new ExceptionNameFixer()->process($violations[0]);
+
+        $result = new FixApplier()->apply($source, [$fix]);
+
+        self::assertSame('<root><exceptionname>RuntimeException</exceptionname></root>', $result->file->content);
+        self::assertSame(1, $result->applied);
+    }
+
+    #[Test]
+    public function itPreservesClassnameAttributes(): void
+    {
+        $content = '<root><classname linkend="runtime-exception">RuntimeException</classname></root>';
+        $document = $this->createDocument($content);
+        $source = new File('file.xml', $content);
+
+        $violations = new ExceptionNameSniff(RunMode::Fix)->process($document, $source);
+
+        self::assertCount(1, $violations);
+        self::assertSame(
+            '<classname linkend="runtime-exception">RuntimeException</classname>',
+            $violations[0]->content,
+        );
+
+        $fix = new ExceptionNameFixer()->process($violations[0]);
+
+        $result = new FixApplier()->apply($source, [$fix]);
+
+        self::assertSame(
+            '<root><exceptionname linkend="runtime-exception">RuntimeException</exceptionname></root>',
+            $result->file->content,
+        );
+        self::assertSame(1, $result->applied);
+    }
+
+    #[Test]
+    public function itKeepsSourceContentAlignedAfterRegularClassnames(): void
+    {
+        $content = '<root><classname>RegularClass</classname><classname>RuntimeException</classname></root>';
+        $document = $this->createDocument($content);
+        $source = new File('file.xml', $content);
+
+        $violations = new ExceptionNameSniff(RunMode::Fix)->process($document, $source);
+
+        $sourceContent = '<classname>RuntimeException</classname>';
+        $beginOffset = (int) strpos($content, $sourceContent);
+
+        self::assertCount(1, $violations);
+        self::assertSame($sourceContent, $violations[0]->content);
+        self::assertSame($beginOffset, $violations[0]->beginOffset);
+        self::assertSame($beginOffset + strlen($sourceContent), $violations[0]->untilOffset);
+
+        $fix = new ExceptionNameFixer()->process($violations[0]);
+
+        $result = new FixApplier()->apply($source, [$fix]);
+
+        self::assertSame(
+            '<root><classname>RegularClass</classname><exceptionname>RuntimeException</exceptionname></root>',
+            $result->file->content,
+        );
+        self::assertSame(1, $result->applied);
+    }
+
+    private function createDocument(string $xml): \DOMDocument
+    {
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+
+        return $document;
+    }
+}

tests/Integration/Fix/WhitespaceConcernFixersTest.php
@@ -1,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Integration\Fix;
+
+use DocbookCS\Fix\Fix;
+use DocbookCS\Fix\FixApplier;
+use DocbookCS\Fix\FixPlan;
+use DocbookCS\Fix\Fixer\MixedIndentationFixer;
+use DocbookCS\Fix\Fixer\TrailingWhitespaceFixer;
+use DocbookCS\Fix\FixResult;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Sniff\MixedIndentationSniff;
+use DocbookCS\Sniff\TrailingWhitespaceSniff;
+use DocbookCS\Source\File;
+use DocbookCS\Source\Line;
+use DocbookCS\Violation\SourceRange;
+use DocbookCS\Violation\Violation;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[
+    CoversClass(Fix::class),
+    CoversClass(FixApplier::class),
+    CoversClass(FixResult::class),
+    CoversClass(MixedIndentationFixer::class),
+    CoversClass(MixedIndentationSniff::class),
+    CoversClass(TrailingWhitespaceFixer::class),
+    CoversClass(TrailingWhitespaceSniff::class),
+    //
+    UsesClass(File::class),
+    UsesClass(FixPlan::class),
+    UsesClass(Line::class),
+    UsesClass(SourceRange::class),
+    UsesClass(Violation::class),
+]
+final class WhitespaceConcernFixersTest extends TestCase
+{
+    #[Test]
+    public function itFixesIndependentWhitespaceConcernsTogether(): void
+    {
+        $content = "<root> \n \t<tag/>  \n</root>";
+        $document = new \DOMDocument();
+        $document->loadXML($content);
+        $source = new File('file.xml', $content);
+
+        $trailingViolations = new TrailingWhitespaceSniff(RunMode::Fix)->process($document, $source);
+        $indentationViolations = new MixedIndentationSniff(RunMode::Fix)->process($document, $source);
+
+        self::assertCount(2, $trailingViolations);
+        self::assertCount(1, $indentationViolations);
+
+        $fixes = [];
+        $trailingFixer = new TrailingWhitespaceFixer();
+        foreach ($trailingViolations as $violation) {
+            $fixes[] = $trailingFixer->process($violation);
+        }
+
+        $indentationFixer = new MixedIndentationFixer();
+        foreach ($indentationViolations as $violation) {
+            $fixes[] = $indentationFixer->process($violation);
+        }
+
+        $result = new FixApplier()->apply($source, $fixes);
+
+        self::assertSame("<root>\n  <tag/>\n</root>", $result->file->content);
+        self::assertSame(3, $result->applied);
+        self::assertSame(0, $result->skipped);
+    }
+}

tests/Integration/Runner/XmlFileProcessorPipelineTest.php
@@ -1,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Integration\Runner;
+
+use DocbookCS\Runner\EntityPreprocessor;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Runner\XmlFileProcessor;
+use DocbookCS\Report\FileReport;
+use DocbookCS\Sniff\AttributeOrderSniff;
+use DocbookCS\Sniff\SniffInterface;
+use DocbookCS\Source\File;
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversNothing]
+final class XmlFileProcessorPipelineTest extends TestCase
+{
+    #[Test]
+    public function itKeepsTheActualSourcePathInViolations(): void
+    {
+        $workingDirectory = getcwd();
+        self::assertIsString($workingDirectory);
+
+        $filePath = tempnam($workingDirectory, 'docbook-cs-');
+        self::assertIsString($filePath);
+
+        try {
+            file_put_contents($filePath, '<root xmlns="urn:test" xml:id="root"/>');
+
+            $report = $this->process(
+                $this->processor([new AttributeOrderSniff()]),
+                '<root xmlns="urn:test" xml:id="root"/>',
+                $filePath,
+            );
+
+            self::assertCount(1, $report->getViolations());
+            self::assertSame($filePath, $report->getViolations()[0]->filePath);
+            self::assertSame($filePath, $report->filePath);
+        } finally {
+            @unlink($filePath);
+        }
+    }
+
+    #[Test]
+    public function itAppliesFixesToTheOriginalSourceWhenEntitiesExpandBeforeTheViolation(): void
+    {
+        $filePath = tempnam(sys_get_temp_dir(), 'docbook-cs-');
+        self::assertIsString($filePath);
+        $source = '<root>&prefix;<tag xmlns="urn:test" xml:id="id"/></root>';
+
+        try {
+            file_put_contents($filePath, $source);
+
+            $processor = $this->processor(
+                [new AttributeOrderSniff(RunMode::Fix)],
+                new EntityPreprocessor([
+                    'prefix' => 'expanded-content-before-tag',
+                ]),
+            );
+
+            $this->processFile($processor, $filePath);
+
+            self::assertSame(
+                '<root>&prefix;<tag xml:id="id" xmlns="urn:test"/></root>',
+                file_get_contents($filePath),
+            );
+        } finally {
+            @unlink($filePath);
+        }
+    }
+
+    private function process(XmlFileProcessor $processor, string $content, string $path = 'input.xml'): FileReport
+    {
+        return $processor->process(new File($path, $content))->fileReport;
+    }
+
+    private function processFile(XmlFileProcessor $processor, string $path): FileReport
+    {
+        $content = file_get_contents($path);
+        self::assertIsString($content);
+
+        $result = $processor->process(new File($path, $content));
+        if ($result->isModified()) {
+            file_put_contents($path, $result->fixedContent());
+        }
+
+        return $result->fileReport;
+    }
+
+    /** @param list<SniffInterface> $sniffs */
+    private function processor(array $sniffs = [], ?EntityPreprocessor $pre = null): XmlFileProcessor
+    {
+        return new XmlFileProcessor(
+            $sniffs,
+            $pre ?? new EntityPreprocessor([]) // always pass array
+        );
+    }
+}

php/docbook-cs

tests/Unit/Fix/ExceptionNameFixerTest.php
@@ -1,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Unit\Fix;
+
+use DocbookCS\Fix\Fix;
+use DocbookCS\Fix\FixApplier;
+use DocbookCS\Fix\FixPlan;
+use DocbookCS\Fix\Fixer\ExceptionNameFixer;
+use DocbookCS\Fix\FixResult;
+use DocbookCS\Runner\EntityExpansionMarker;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Sniff\ExceptionNameSniff;
+use DocbookCS\Source\File;
+use DocbookCS\Source\Line;
+use DocbookCS\Violation\SourceRange;
+use DocbookCS\Violation\Violation;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[
+    CoversClass(ExceptionNameFixer::class),
+    CoversClass(ExceptionNameSniff::class),
+    CoversClass(Fix::class),
+    CoversClass(FixApplier::class),
+    CoversClass(FixResult::class),
+    CoversClass(RunMode::class),
+    CoversClass(Violation::class),
+    //
+    UsesClass(EntityExpansionMarker::class),
+    UsesClass(File::class),
+    UsesClass(FixPlan::class),
+    UsesClass(Line::class),
+    UsesClass(SourceRange::class),
+]
+final class ExceptionNameFixerTest extends TestCase
+{
+    #[Test]
+    public function itReplacesSimpleClassnameTags(): void
+    {
+        $content = '<root><classname>RuntimeException</classname></root>';
+        $document = $this->createDocument($content);
+        $source = new File('file.xml', $content);
+
+        $violations = new ExceptionNameSniff(RunMode::Fix)->process($document, $source);
+
+        $beginOffset = (int) strpos($content, '<classname>');
+        $sourceContent = '<classname>RuntimeException</classname>';
+
+        self::assertCount(1, $violations);
+        self::assertSame($sourceContent, $violations[0]->content);
+        self::assertSame($beginOffset, $violations[0]->beginOffset);
+        self::assertSame($beginOffset + strlen($sourceContent), $violations[0]->untilOffset);
+        self::assertSame(1, $violations[0]->line);
+
+        $fix = new ExceptionNameFixer()->process($violations[0]);
+
+        $result = new FixApplier()->apply($source, [$fix]);
+
+        self::assertSame('<root><exceptionname>RuntimeException</exceptionname></root>', $result->file->content);
+        self::assertSame(1, $result->applied);
+    }
+
+    #[Test]
+    public function itPreservesClassnameAttributes(): void
+    {
+        $content = '<root><classname linkend="runtime-exception">RuntimeException</classname></root>';
+        $document = $this->createDocument($content);
+        $source = new File('file.xml', $content);
+
+        $violations = new ExceptionNameSniff(RunMode::Fix)->process($document, $source);
+
+        self::assertCount(1, $violations);
+        self::assertSame(
+            '<classname linkend="runtime-exception">RuntimeException</classname>',
+            $violations[0]->content,
+        );
+
+        $fix = new ExceptionNameFixer()->process($violations[0]);
+
+        $result = new FixApplier()->apply($source, [$fix]);
+
+        self::assertSame(
+            '<root><exceptionname linkend="runtime-exception">RuntimeException</exceptionname></root>',
+            $result->file->content,
+        );
+        self::assertSame(1, $result->applied);
+    }
+
+    #[Test]
+    public function itKeepsSourceContentAlignedAfterRegularClassnames(): void
+    {
+        $content = '<root><classname>RegularClass</classname><classname>RuntimeException</classname></root>';
+        $document = $this->createDocument($content);
+        $source = new File('file.xml', $content);
+
+        $violations = new ExceptionNameSniff(RunMode::Fix)->process($document, $source);
+
+        $sourceContent = '<classname>RuntimeException</classname>';
+        $beginOffset = (int) strpos($content, $sourceContent);
+
+        self::assertCount(1, $violations);
+        self::assertSame($sourceContent, $violations[0]->content);
+        self::assertSame($beginOffset, $violations[0]->beginOffset);
+        self::assertSame($beginOffset + strlen($sourceContent), $violations[0]->untilOffset);
+
+        $fix = new ExceptionNameFixer()->process($violations[0]);
+
+        $result = new FixApplier()->apply($source, [$fix]);
+
+        self::assertSame(
+            '<root><classname>RegularClass</classname><exceptionname>RuntimeException</exceptionname></root>',
+            $result->file->content,
+        );
+        self::assertSame(1, $result->applied);
+    }
+
+    private function createDocument(string $xml): \DOMDocument
+    {
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+
+        return $document;
+    }
+}

tests/Unit/Fix/WhitespaceConcernFixersTest.php
@@ -1,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Unit\Fix;
+
+use DocbookCS\Fix\Fix;
+use DocbookCS\Fix\FixApplier;
+use DocbookCS\Fix\FixPlan;
+use DocbookCS\Fix\Fixer\MixedIndentationFixer;
+use DocbookCS\Fix\Fixer\TrailingWhitespaceFixer;
+use DocbookCS\Fix\FixResult;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Sniff\MixedIndentationSniff;
+use DocbookCS\Sniff\TrailingWhitespaceSniff;
+use DocbookCS\Source\File;
+use DocbookCS\Source\Line;
+use DocbookCS\Violation\SourceRange;
+use DocbookCS\Violation\Violation;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[
+    CoversClass(Fix::class),
+    CoversClass(FixApplier::class),
+    CoversClass(FixResult::class),
+    CoversClass(MixedIndentationFixer::class),
+    CoversClass(MixedIndentationSniff::class),
+    CoversClass(TrailingWhitespaceFixer::class),
+    CoversClass(TrailingWhitespaceSniff::class),
+    //
+    UsesClass(File::class),
+    UsesClass(FixPlan::class),
+    UsesClass(Line::class),
+    UsesClass(SourceRange::class),
+    UsesClass(Violation::class),
+]
+final class WhitespaceConcernFixersTest extends TestCase
+{
+    #[Test]
+    public function itFixesIndependentWhitespaceConcernsTogether(): void
+    {
+        $content = "<root> \n \t<tag/>  \n</root>";
+        $document = new \DOMDocument();
+        $document->loadXML($content);
+        $source = new File('file.xml', $content);
+
+        $trailingViolations = new TrailingWhitespaceSniff(RunMode::Fix)->process($document, $source);
+        $indentationViolations = new MixedIndentationSniff(RunMode::Fix)->process($document, $source);
+
+        self::assertCount(2, $trailingViolations);
+        self::assertCount(1, $indentationViolations);
+
+        $fixes = [];
+        $trailingFixer = new TrailingWhitespaceFixer();
+        foreach ($trailingViolations as $violation) {
+            $fixes[] = $trailingFixer->process($violation);
+        }
+
+        $indentationFixer = new MixedIndentationFixer();
+        foreach ($indentationViolations as $violation) {
+            $fixes[] = $indentationFixer->process($violation);
+        }
+
+        $result = new FixApplier()->apply($source, $fixes);
+
+        self::assertSame("<root>\n  <tag/>\n</root>", $result->file->content);
+        self::assertSame(3, $result->applied);
+        self::assertSame(0, $result->skipped);
+    }
+}

tests/Unit/Runner/XmlFileProcessorPipelineTest.php
@@ -1,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocbookCS\Tests\Unit\Runner;
+
+use DocbookCS\Runner\EntityPreprocessor;
+use DocbookCS\Runner\RunMode;
+use DocbookCS\Runner\XmlFileProcessor;
+use DocbookCS\Report\FileReport;
+use DocbookCS\Sniff\AttributeOrderSniff;
+use DocbookCS\Sniff\SniffInterface;
+use DocbookCS\Source\File;
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversNothing]
+final class XmlFileProcessorPipelineTest extends TestCase
+{
+    #[Test]
+    public function itKeepsTheActualSourcePathInViolations(): void
+    {
+        $workingDirectory = getcwd();
+        self::assertIsString($workingDirectory);
+
+        $filePath = tempnam($workingDirectory, 'docbook-cs-');
+        self::assertIsString($filePath);
+
+        try {
+            file_put_contents($filePath, '<root xmlns="urn:test" xml:id="root"/>');
+
+            $report = $this->process(
+                $this->processor([new AttributeOrderSniff()]),
+                '<root xmlns="urn:test" xml:id="root"/>',
+                $filePath,
+            );
+
+            self::assertCount(1, $report->getViolations());
+            self::assertSame($filePath, $report->getViolations()[0]->filePath);
+            self::assertSame($filePath, $report->filePath);
+        } finally {
+            @unlink($filePath);
+        }
+    }
+
+    #[Test]
+    public function itAppliesFixesToTheOriginalSourceWhenEntitiesExpandBeforeTheViolation(): void
+    {
+        $filePath = tempnam(sys_get_temp_dir(), 'docbook-cs-');
+        self::assertIsString($filePath);
+        $source = '<root>&prefix;<tag xmlns="urn:test" xml:id="id"/></root>';
+
+        try {
+            file_put_contents($filePath, $source);
+
+            $processor = $this->processor(
+                [new AttributeOrderSniff(RunMode::Fix)],
+                new EntityPreprocessor([
+                    'prefix' => 'expanded-content-before-tag',
+                ]),
+            );
+
+            $this->processFile($processor, $filePath);
+
+            self::assertSame(
+                '<root>&prefix;<tag xml:id="id" xmlns="urn:test"/></root>',
+                file_get_contents($filePath),
+            );
+        } finally {
+            @unlink($filePath);
+        }
+    }
+
+    private function process(XmlFileProcessor $processor, string $content, string $path = 'input.xml'): FileReport
+    {
+        return $processor->process(new File($path, $content))->fileReport;
+    }
+
+    private function processFile(XmlFileProcessor $processor, string $path): FileReport
+    {
+        $content = file_get_contents($path);
+        self::assertIsString($content);
+
+        $result = $processor->process(new File($path, $content));
+        if ($result->isModified()) {
+            file_put_contents($path, $result->fixedContent());
+        }
+
+        return $result->fileReport;
+    }
+
+    /** @param list<SniffInterface> $sniffs */
+    private function processor(array $sniffs = [], ?EntityPreprocessor $pre = null): XmlFileProcessor
+    {
+        return new XmlFileProcessor(
+            $sniffs,
+            $pre ?? new EntityPreprocessor([]) // always pass array
+        );
+    }
+}